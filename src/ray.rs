7d910e9e19de5d08ade2c30c055831d8e7f6982a
canmom <bryn.dickinson@gmail.com>
2019-06-01 22:05:15 +0100

    Add representations of rays and lights

---
+++ src/ray.rs
@@ -0,0 +1,31 @@
+use crate::vector::{Vec2, Vec3, dot};
+
+#[derive(Debug)]
+pub struct Light {
+    pub loc: Vec2<f64>,
+    pub col: Vec3<f64>,
+}
+
+#[derive(Debug)]
+pub struct Ray<'a> {
+    origin: Vec2<f64>,
+    dir: Vec2<f64>,
+    length: f64,
+    target: &'a Light,
+}
+
+impl<'a> Ray<'a> {
+    pub fn new(from: Vec2<f64>, to: &Light) -> Ray {
+        let fullvector = to.loc - from;
+        Ray {
+            origin: from,
+            dir: fullvector.normalise(),
+            length: fullvector.length(),
+            target: &to,
+        }
+    }
+
+    pub fn shade(&self) -> Vec3<f64> {
+        self.target.col / (self.length * self.length)
+    }
+}
